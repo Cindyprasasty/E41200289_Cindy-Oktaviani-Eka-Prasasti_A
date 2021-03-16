b5705f2a4a35717ef8a44c076a2aa5fe4def7c14
Cindyprasasty <Cindy Oktaviani Eka Prasasti>
2021-03-16 19:28:56 +0700

    LATIHAN

---
+++ Minggu 2/LATIHAN/Materi3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan_Java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Materi3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s1 = new Scanner (System.in);
+        
+        System.out.print("Masukan angka1 = ");
+        int angka1 = s1.nextInt();
+        System.out.print("Masukan angka2 = ");
+        int angka2 = s1.nextInt();
+        
+        int hasil = angka1 + angka2;
+        System.out.println("Hasil = "+hasil);
+    }
+    
+}
