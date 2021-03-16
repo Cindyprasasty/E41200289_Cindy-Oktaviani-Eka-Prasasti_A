bc6b5a2cd3c5446726784a17ef2e10284178e4dd
Cindyprasasty <Cindy Oktaviani Eka Prasasti>
2021-03-16 19:30:36 +0700

    TUGAS

---
+++ Minggu 2/TUGAS/person_tugas1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author HP
+ */
+public class person_tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    
+    String fName = "Cindy Oktaviani";
+    String lName = "Eka Prasasti";
+    String stuId = "E41200289";
+    String stuStatus = "Active";
+    
+    System.out.println("Student Name = "+ fName + " " + lName);
+    System.out.println("Student ID = "+ stuId);
+    System.out.println("Student Status = "+ stuStatus);
+    }
+    
+}
